fa377ce3b04a54224f59fb89fad7819ab42651e1
Will Thomas <willthomas@Wills-MBP.lan>
2024-11-27 15:51:45 -0600

    functional haskell gone

--- functional_haskell/PS/ps1/Sumf.hs
+++
@@ -1,9 +0,0 @@
-
-module Sumf where
-
-sumf :: Num n => (a -> n) -> [a] -> n
-sumf f [] = 0
-sumf f (c:cs) = f c + sumf f cs
-
-square :: Num n => n -> n
-square x = x^2
--- functional_haskell/PS/ps2/6.1.hs
+++
@@ -1,4 +0,0 @@
-data Pair a b = Pair a b 
-
-instance (ord a, ord b) => Ord (Pair a b) where 
-    Pair a1 b1 <= Pair a2 b2 = a1 < a2 || a1 == a2 
